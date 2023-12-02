6d281a9a0cc8489cefbe9c7f137d5dab50db6562
hossein karimy <hosseinkm01@gmail.com>
2023-12-02 13:18:07 +0330

    Add Some Results

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,7 +1,7 @@
 ﻿using HillClimbing;
 
 Heritage.Initialize();
-var result = await HillClimbing_Steepest.TryClimbing(1000, 4);
+var result = await HillClimbing_Steepest.TryClimbing(10000, 4);
 //await HillClimbing_Lazy.TryClimbing(TimeSpan.FromSeconds(10), 4);
 //await HillClimbing_Stochastic.TryClimbing(TimeSpan.FromSeconds(100),2, result);
 
